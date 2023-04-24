452456a6ba615e6bfe2aef10c6044daa290a17ca
Julio Reyes <juliojesusreyes@gmail.com>
2023-04-24 17:56:06 -0400

    Comenzando molde de tarjeta.

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
 // Inicio
 
+// Elementos del DOM.
+
+const contenedorHijo = document.querySelector (".contenedorHijo");
+const tarjetaInformacion = document.getElementById ("tarjetaInformacion");
+const personajes = document.getElementById ("botonPersonajes");
+const sitios = document.getElementById ("botonSitios");
+const episodios = document.getElementById ("botonEpisodios");
+
+// Logica.
+
+const url = "https://rickandmortyapi.com/api/character";
+
+const listadoPersonajes = fetch (url).then ((respuesta) => {
+
+return respuesta.json ();
+
+})
+
+.then ((data) => {
+
+    let personajes = data;
+    console.log (personajes);
+
+});
+
+
 
 
 // Fin
