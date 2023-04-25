fc21c32000ed63d3c30b5f2b869906da7b6ea241
Julio Reyes <juliojesusreyes@gmail.com>
2023-04-25 17:37:01 -0400

    Terminando primera revision del proyecto.

---
+++ beths.js
@@ -0,0 +1,76 @@
+// Inicio
+
+// Elementos del DOM.
+
+const contenedorHijo = document.querySelector (".contenedorHijo");
+
+// Cada boton muestra resultados distintos.
+
+// Mostrando a los Rick.
+
+// Obteniendo datos desde la API.
+
+function obtenerPersonajes (listo) {
+
+    let url = "https://rickandmortyapi.com/api/character/?name=beth";
+    const resultado = fetch (url);
+
+    resultado
+        .then(response => response.json())
+        .then(data => {
+
+            listo(data);
+
+        });
+
+};
+
+// Definiendo la manera en que se muestran los datos.
+
+obtenerPersonajes (data => {
+
+    data.results.forEach(personaje => {
+
+        const tarjetaInformacion = document.createElement ("div");
+        tarjetaInformacion.setAttribute ("style", "background-color: black; height: 13rem; width: 30rem");
+        tarjetaInformacion.setAttribute ("class", "rounded-3 d-flex flex-row align-items-center");
+        const contenido = 
+            `
+                <img style="height: 13rem; width: 12rem;" class="rounded-start-3" src="${personaje.image}">
+    
+                <div class="d-flex flex-column ps-3 gap-1">
+                <a class="text-decoration-none text-warning" href="${personaje.url}"><span class="fs-4 text-info fw-semibold">${personaje.name}
+                </a></span>
+                    <div>
+                        <span class="text-light fs-5 fw-semibold">Status: ${personaje.status} Species: ${personaje.species}</span>
+                    </div>
+                    <div class="d-flex flex-column gap-1">
+                        <span class="fs-5 fw-bolder text-light">Last known location:</span>
+                        <a class="text-decoration-none text-warning" href="${personaje.location.url}"><span class="fs-6 text-info fw-semibold">${personaje.location.name}
+                        </a></span>
+                    </div>
+    
+                </div>
+            `;
+        tarjetaInformacion.innerHTML = contenido;
+        contenedorHijo.appendChild (tarjetaInformacion);
+        
+    });
+        
+});
+
+Swal.fire ({
+
+    toast: "true",
+    position: "center",
+    icon: "info",
+    color: "black",
+    text: "Mostrando todas la Beth.",
+    showConfirmButton: false,
+    timer: 2500,
+
+});
+
+
+// Fin
+// Julio Reyes 25ABR2023
